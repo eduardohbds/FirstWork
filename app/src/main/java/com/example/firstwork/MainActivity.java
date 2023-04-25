f3699cff489dbc8eca91553766baebfd922d6c1c
eduardo <eduardo.hbds@gmail.com>
2023-04-25 10:52:01 -0300

    fix: main activity

--- app/src/main/java/com/example/firstwork/MainActivity.java
+++ app/src/main/java/com/example/firstwork/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.firstwork;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -12,20 +16,57 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.firstwork.Adapters.NoteListAdapter;
+import com.example.firstwork.Database.RoomDB;
+import com.example.firstwork.Models.Notes;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> items;
-    ListView list;
-    Button button;
-    ArrayAdapter<String> itemAdapter;
-
+    RecyclerView recyclerView;
+    NoteListAdapter noteListAdapter;
+    List<Notes> notes = new ArrayList<>();
+    RoomDB database;
+    FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycler_home);
+        floatingActionButton = findViewById(R.id.fab_add);
 
+        database = RoomDB.getInstance(this);
+        notes = database.mainDAO().getAll();
 
+        updateRecycler(notes);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+    private void updateRecycler(List<Notes> notes) {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
+        noteListAdapter = new NoteListAdapter(MainActivity.this,notes,notesClickListener);
+        recyclerView.setAdapter(noteListAdapter);
+    }
+
+    private final NotesClickListener notesClickListener = new NotesClickListener() {
+        @Override
+        public void onClick(Notes notes) {
+
+        }
+
+        @Override
+        public void onLongClick(Notes notes, CardView cardView) {
+
+        }
+    };
 }
 
