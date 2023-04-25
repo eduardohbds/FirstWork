56aa3d0e34d57cfe372b339b50f47f317badb408
Eduardo <eduardo.hbds@gmail.com>
2023-04-25 09:56:02 -0300

    feat: create list adapter

---
+++ app/src/main/java/com/example/firstwork/Adapters/NoteListAdapter.java
@@ -0,0 +1,104 @@
+package com.example.firstwork.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.firstwork.Models.Notes;
+import com.example.firstwork.NotesClickListener;
+import com.example.firstwork.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class NoteListAdapter extends RecyclerView.Adapter<NoteViewHolder>{
+    Context context;
+    List<Notes> list;
+    NotesClickListener listener;
+
+    public NoteListAdapter(Context context, List<Notes> list, NotesClickListener listener) {
+        this.context = context;
+        this.list = list;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NoteViewHolder(LayoutInflater.from(context).inflate(R.layout.notes_list,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.textView_title.setText(list.get(position).getTitle());
+        holder.textView_title.setSelected(true);
+
+        holder.textView_notes.setText(list.get(position).getNotes());
+
+        holder.textView_date.setText(list.get(position).getData());
+        holder.textView_date.setSelected(true);
+
+        if (list.get(position).isPinned()){
+            holder.imageView_pin.setImageResource(R.drawable.ic_pin);
+        }else {
+            holder.imageView_pin.setImageResource(0);
+        }
+
+        int colorCode = getRandomColor();
+        holder.notes_container.setCardBackgroundColor(holder.itemView.getResources().getColor(colorCode,null));
+
+        holder.notes_container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClick(list.get(holder.getAdapterPosition()));
+            }
+        });
+        holder.notes_container.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                listener.onLongClick(list.get(holder.getAdapterPosition()),holder.notes_container);
+                return true;
+            }
+        });
+    }
+
+    public int getRandomColor() {
+        List <Integer> colorCode = new ArrayList<>();
+        colorCode.add(R.color.color1);
+        colorCode.add(R.color.color2);
+        colorCode.add(R.color.color3);
+        colorCode.add(R.color.color4);
+        colorCode.add(R.color.color5);
+
+        Random random = new Random();
+        int random_color = random.nextInt(colorCode.size());
+        return random_color;
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
+class NoteViewHolder extends RecyclerView.ViewHolder {
+    CardView notes_container;
+    TextView textView_title,textView_notes,textView_date;
+    ImageView imageView_pin;
+    public NoteViewHolder(@NonNull View itemView) {
+        super(itemView);
+        notes_container = itemView.findViewById(R.id.notes_container);
+        textView_title = itemView.findViewById(R.id.textView_title);
+        textView_notes = itemView.findViewById(R.id.textView_notes);
+        textView_date = itemView.findViewById(R.id.textView_date);
+        imageView_pin = itemView.findViewById(R.id.imageView_pin);
+
+    }
+}
--- app/src/main/java/com/example/firstwork/MainActivity.java
+++ app/src/main/java/com/example/firstwork/MainActivity.java
@@ -19,49 +19,13 @@ public class MainActivity extends AppCompatActivity {
     ListView list;
     Button button;
     ArrayAdapter<String> itemAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        list = findViewById(R.id.list);
-        button = findViewById(R.id.button);
-        
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addItem(v);
-            }
-        });
-        items = new ArrayList<>();
-        itemAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items);
-        list.setAdapter(itemAdapter);
-        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                return removeItem(position);
-            }
-        });
 
-    }
-
-    private boolean removeItem(int position) {
-        Context context = getApplicationContext();
-        items.remove(position);
-        itemAdapter.notifyDataSetChanged();
-        Toast.makeText(context,"Item removed",Toast.LENGTH_LONG).show();
-        return true;
-    }
-
-    private void addItem(View v) {
-        EditText input = findViewById(R.id.edit_text);
-        String itemText = input.getText().toString();
-
-        if (!(itemText.equals(""))){
-            itemAdapter.add(itemText);
-            input.setText("");
-        }else {
-            Toast.makeText(getApplicationContext(),"Please enter text...",Toast.LENGTH_LONG ).show();
-        }
     }
 }
+
